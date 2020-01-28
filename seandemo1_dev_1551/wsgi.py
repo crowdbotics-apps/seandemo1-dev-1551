3dae65a4ab1737b79aa02e26c00fe64cd0928e5a
Crowdbotics <team@crowdbotics.com>
2020-01-28 19:48:05 +0000

    Initial commit.

---
+++ seandemo1_dev_1551/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for seandemo1_dev_1551 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seandemo1_dev_1551.settings')
+
+application = get_wsgi_application()
